dc23b6421717913a0a9f315922284658e8c280c2
MrBs <Chouaibwidadipur@gmail.com>
2023-10-09 23:20:39 +0100

    Removed consol logs and fixed theme toggle

--- app/users/page.tsx
+++ app/users/page.tsx
@@ -4,7 +4,6 @@ import { columns } from "./Columns";
 
 export default async function Home() {
   const users = await getUsers();
-  console.log(users);
   return (
     <div className="w-full h-full sm:p-10 p-4">
       <UsersDataTable data={users} columns={columns} />
--- components/ThemeToggle.tsx
+++ components/ThemeToggle.tsx
@@ -2,9 +2,10 @@
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { motion } from "framer-motion";
+import { useEffect } from "react";
 
 export default function ThemeToggle() {
-  const { theme, setTheme } = useTheme();
+  const { theme, setTheme, systemTheme } = useTheme();
   const handleThemeChange = () => {
     setTheme(theme == "light" ? "dark" : "light");
   };
@@ -30,43 +31,45 @@ export default function ThemeToggle() {
       className="p-6 rounded-full w-max outline outline-2 outline-foreground flex cursor-pointer"
       onClick={handleThemeChange}
     >
-      {theme == "light" && (
-        <motion.div
-          variants={variants}
-          initial="initial"
-          animate="animate"
-          exit="exit"
-          transition={{
-            duration: 0.5,
-            type: "spring",
-            stiffness: 100,
-            damping: 10,
-            mass: 1,
-          }}
-          className="absolute m-auto"
-        >
-          <Sun className="w-full h-full" />
-        </motion.div>
-      )}
+      {theme == "light" ||
+        (systemTheme == "light" && (
+          <motion.div
+            variants={variants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.5,
+              type: "spring",
+              stiffness: 100,
+              damping: 10,
+              mass: 1,
+            }}
+            className="absolute m-auto"
+          >
+            <Sun className="w-full h-full" />
+          </motion.div>
+        ))}
 
-      {theme == "dark" && (
-        <motion.div
-          variants={variants}
-          initial="initial"
-          animate="animate"
-          exit="exit"
-          transition={{
-            duration: 0.5,
-            type: "spring",
-            stiffness: 100,
-            damping: 10,
-            mass: 1,
-          }}
-          className="absolute m-auto -translate-x-1/2 -translate-y-1/2"
-        >
-          <Moon className="w-full h-ful" />
-        </motion.div>
-      )}
+      {theme == "dark" ||
+        (systemTheme == "dark" && (
+          <motion.div
+            variants={variants}
+            initial="initial"
+            animate="animate"
+            exit="exit"
+            transition={{
+              duration: 0.5,
+              type: "spring",
+              stiffness: 100,
+              damping: 10,
+              mass: 1,
+            }}
+            className="absolute m-auto -translate-x-1/2 -translate-y-1/2"
+          >
+            <Moon className="w-full h-ful" />
+          </motion.div>
+        ))}
     </div>
   );
 }
